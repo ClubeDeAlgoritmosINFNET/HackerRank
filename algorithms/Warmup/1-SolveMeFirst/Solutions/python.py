7a520e53c1e8bacf2e1836d64ef5d44770315f16
Leonardo Cabral Evaristo <leocabrallce@gmail.com>
2018-03-12 15:52:41 -0300

    Update python.py

--- algorithms/Warmup/1-SolveMeFirst/Solutions/python.py
+++ algorithms/Warmup/1-SolveMeFirst/Solutions/python.py
@@ -1,9 +1,7 @@
-def solveMeFirst(a,b):
+def solveMeFirst(a, b):
    return a+b
   
-  
-
 num1 = int(input())
 num2 = int(input())
-res = solveMeFirst(num1,num2)
+res = solveMeFirst(num1, num2)
 print(res)
