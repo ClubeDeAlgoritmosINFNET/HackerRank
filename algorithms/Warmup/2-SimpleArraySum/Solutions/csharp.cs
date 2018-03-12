4a15c7444b20719cb0c061986e904591fe903a8f
Leonardo Cabral Evaristo <leocabrallce@gmail.com>
2018-03-12 15:56:04 -0300

    Update csharp.cs

--- algorithms/Warmup/2-SimpleArraySum/Solutions/csharp.cs
+++ algorithms/Warmup/2-SimpleArraySum/Solutions/csharp.cs
@@ -2,19 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-class Solution {
 
+class Solution {
     static int simpleArraySum(int n, int[] ar) {
         int  sum = 0;
-        
-        
         for(int i = 0; i < n ; i++){
             sum = sum + ar[i];
         }
-        
         return sum;
     }
-
     static void Main(String[] args) {
         int n = Convert.ToInt32(Console.ReadLine());
         string[] ar_temp = Console.ReadLine().Split(' ');
