227ea55c488840af97a84816c7c5845b60d83c90
Leonardo Cabral Evaristo <leocabrallce@gmail.com>
2018-03-12 15:57:16 -0300

    Update csharp.cs

--- algorithms/Warmup/1-SolveMeFirst/Solutions/csharp.cs
+++ algorithms/Warmup/1-SolveMeFirst/Solutions/csharp.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+
 class Solution {
     static int solveMeFirst(int a, int b) { 
       return a+b;
-      
     }
     static void Main(String[] args) {
         int val1 = Convert.ToInt32(Console.ReadLine());
@@ -12,4 +12,4 @@ class Solution {
         int sum = solveMeFirst(val1,val2);
         Console.WriteLine(sum);
     }
-}      
+}
